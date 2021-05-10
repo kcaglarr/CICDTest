9a6365aa7036041d316db7511f4803b5a5178774
Kerim Caglar <kerim@marketyo.com>
2021-05-10 10:44:16 +0300

    first commit

---
+++ CICDTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CICDTest
+//
+//  Created by Kerim Çağlar on 10.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    func sum(x:Int, y:Int) -> Int {
+        return x + y
+    }
+
+    func multiply(x:Int, y:Int) -> Int {
+        return x * y
+    }
+}
+
---
+++ CICDTestTests/CICDTestTests.swift
@@ -0,0 +1,33 @@
+//
+//  CICDTestTests.swift
+//  CICDTestTests
+//
+//  Created by Kerim Çağlar on 10.05.2021.
+//
+
+import XCTest
+@testable import CICDTest
+
+class CICDTestTests: XCTestCase {
+
+    var viewController : ViewController!
+    
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        viewController = ViewController()
+        
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func sumTest() throws {
+        XCTAssertEqual(viewController.sum(x: 3, y: 5), 8)
+    }
+
+    func multiplyTest() throws {
+        XCTAssertEqual(viewController.sum(x: 3, y: 5), 15)
+    }
+
+}
